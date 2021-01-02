5293b2c15241688bd2678f2e9f1f63c3b28cb047
Jason A. White <jaydoubleyoo@jaydoubleyoo.com>
2021-01-02 14:27:38 -0800

    Auto-deploy test 3

--- includes/inc_hero.php
+++ includes/inc_hero.php
@@ -1,7 +1,7 @@
     <section class="parallax section-module section-hero grid full">
       <div class="container">
       	<div class="row content">
-        	<h1 class="section-hero__title display-heading-1 col-12">Dad Ambassador</h1>
+        	<h1 class="section-hero__title display-heading-1 col-12">Dude Ambassador</h1>
         	<h2 class="section-hero__subtitle col-12">I'm Jason A. White. Visual Creative &amp; UX Engineer based in Los Angeles.</h2>
         	<p class="section-hero__description col-12"><a href="mailto:jw@jaydoubleyoo.com" >HIRE ME</a></p>
       	</div>
