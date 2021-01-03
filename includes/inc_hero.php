12e74ca5dbe350a29dec98832e7f6ddec9057601
Jason A. White <jaydoubleyoo@jaydoubleyoo.com>
2021-01-02 17:04:45 -0800

    Showing Amelle how auto-deploy works

--- includes/inc_hero.php
+++ includes/inc_hero.php
@@ -1,8 +1,8 @@
     <section class="parallax section-module section-hero grid full">
       <div class="container">
       	<div class="row content">
-        	<h1 class="section-hero__title display-heading-1 col-12">Dad Ambassador</h1>
-        	<h2 class="section-hero__subtitle col-12">I'm Jason A. Black. Visual Creative &amp; SUX Engineer based in Los Angeles.</h2>
+        	<h1 class="section-hero__title display-heading-1 col-12">Amelle Simone White</h1>
+        	<h2 class="section-hero__subtitle col-12">I'm Jason A. White. Visual Creative &amp; UX Engineer based in Los Angeles.</h2>
         	<p class="section-hero__description col-12"><a href="mailto:jw@jaydoubleyoo.com" >HIRE ME</a></p>
       	</div>
       </div>
