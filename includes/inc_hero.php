988565511e20b6f88ac4936ea297f6fefda31c47
Jason A. White <jaydoubleyoo@jaydoubleyoo.com>
2021-09-15 02:41:32 -0700

    testing deployment

--- includes/inc_hero.php
+++ includes/inc_hero.php
@@ -2,7 +2,7 @@
       <div class="container">
       	<div class="row content">
         	<h1 class="section-hero__title display-heading-1 col-12">Brand Ambassador</h1>
-        	<h2 class="section-hero__subtitle col-12">I'm Jason A. White. UX Designer &amp; Engineer based in Los Angeles.</h2>
+        	<h2 class="section-hero__subtitle col-12">I'm adsfasd A. White. UX Designer &amp; Engineer based in Los Angeles.</h2>
         	<p class="section-hero__description col-12"><a href="mailto:jw@jaydoubleyoo.com" >HIRE ME</a></p>
       	</div>
       </div>
